f3aa8c03e79af40731b4c69a90c5097bbd6acb64
Daniil Zavyalov <dezavv1@gmail.com>
2024-04-15 16:49:19 +0600

    [~] init project

---
+++ src/main/java/ai/leantech/urlshortener/UrlshortenerApplication.java
@@ -0,0 +1,13 @@
+package ai.leantech.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerApplication.class, args);
+	}
+
+}
