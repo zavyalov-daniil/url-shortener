b665762be999b27d594ad03ea3f9227f48254efb
Daniil Zavyalov <dezavv1@gmail.com>
2024-04-19 14:10:54 +0600

    [~] add status code to exception

--- src/main/java/ai/leantech/urlshortener/common/exception/LongUrlNotFoundException.java
+++ src/main/java/ai/leantech/urlshortener/common/exception/LongUrlNotFoundException.java
@@ -1,3 +1,8 @@
 package ai.leantech.urlshortener.common.exception;
 
-public class LongUrlNotFoundException extends RuntimeException {}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Long url not found", code = HttpStatus.NOT_FOUND)
+public class LongUrlNotFoundException {
+}
